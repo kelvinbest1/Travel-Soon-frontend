cca934fbe2e26f6649024f9738f5ad37f1de63cb
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-12 09:43:45 -0400

    add camps-api.js

---
+++ src/services/camps-api.js
@@ -0,0 +1,15 @@
+const BASE_URL = "/api/camps";
+const APIKey = process.env.REACT_APP_API_KEY
+export default {getAll};
+
+
+export function getAll() {
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json",
+            "APIKey": APIKey
+        },
+    };
+    return fetch(`${BASE_URL}/search`, options).then(res => res.json());
+}

4e9a52f489e7e36b23b999d1599d31ff102c68b4
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-12 09:55:02 -0400

    complete EditTripPage.jsx

--- src/components/EditTripPage/EditTripPage.jsx
+++ src/components/EditTripPage/EditTripPage.jsx
@@ -25,4 +25,59 @@ class EditTripPage extends Component {
             invalidForm: !this.formRef.current.checkValidity(),
         });
     };
-    
+
+    render() {
+        return (
+            <>
+                <h1>Edit Trip</h1>
+                <form
+                    ref={this.formRef}
+                    autoComplete="off"
+                    onSubmit={this.handleSubmit}
+                >
+                    <div className="form-group">
+                        <label>Trip Name</label>
+                        <input 
+                            className="form-control" 
+                            name="name"
+                            value={this.state.formData.name}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>City</label>
+                        <input
+                            className="form-control"
+                            name="city"
+                            value={this.state.formData.city}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>State</label>
+                        <input
+                            className="form-control"
+                            name="state"
+                            value={this.state.formData.state}
+                            onChange={this.handleChange}
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="btn btn-xs"
+                        disabled={this.state.invalidForm}
+                    >Save Trip Changes 
+                    </button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+
+                    <Link to="/">Cancel Changes</Link>
+                </form>
+            </>
+        );
+    }
+}
+export default EditTripPage;
+

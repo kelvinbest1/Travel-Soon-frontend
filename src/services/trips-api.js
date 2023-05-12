bb17078d407958c58f005f2ff94aa8afedcc2b43
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-12 09:42:33 -0400

    complete trips-api.js

--- src/services/trips-api.js
+++ src/services/trips-api.js
@@ -15,3 +15,17 @@ export function create(trip) {
         body: JSON.stringify(trip)
     }).then(res => res.json());
 }
+
+export function update(trip) {
+    return fetch(`${BASE_URL}/${trip._id}`, {
+        method: "PUT",
+        headers: {"content-type": "application/json"},
+        body: JSON.stringify(trip)
+    }).then(res => res.json());
+}
+
+export function deleteTrip(id) {
+    return fetch(`${BASE_URL}/${id}`,{
+        method: "DELETE"
+    }).then(res => res.json());
+}

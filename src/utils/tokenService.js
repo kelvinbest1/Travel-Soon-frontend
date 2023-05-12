0f7deb58bdc8f9ebab4ff30cc11723d96b26cd15
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-12 09:35:10 -0400

    complete tokenService.js

--- src/utils/tokenService.js
+++ src/utils/tokenService.js
@@ -13,3 +13,20 @@ function getUserFromToken() {
     const token = getToken();
     return token ? JSON.parse(atob(token.split(".")[1])).user : null;
 }
+
+function getToken() {
+    let token = localStorage.getItem("token");
+    if (token) {
+        // Check if token expired, remove if it is
+        const payload = JSON.parse(atob(token.split(".")[1]));
+        if (payload.exp < Date.now() / 1000) {
+            localStorage.removeItem("token");
+            token = null;
+        }
+    }
+    return token;
+}
+
+function setToken(token) {
+    localStorage.setItem("token", token);
+}

88501255f8e88881a4e035013992956a31082181
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-17 14:17:25 -0400

    update Signup Form

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,77 +1,106 @@
-import React, { useState } from 'react';
-
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import userService from "../../utils/userService";
 
-function SignUpForm() {
+const SignupForm = ({ updateMessage, handleSignupOrLogin, history }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    passwordConf: "",
+  });
 
-    const [name, setName]= useState('')
-     const [email, setEmail]= useState('')  
-      const [password, setPassword]= useState('') 
-        const [passwordConf, setPasswordconf]= useState('')
-    
+  const handleChange = (e) => {
+    updateMessage("");
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-    handleChange = (e) => {
-        this.props.updateMessage("");
-        this.setState({
-        
-            [e.target.name]: e.target.value
-        });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await userService.signup(formData);
+      handleSignupOrLogin();
+      history.push("/");
+    } catch (err) {
+      updateMessage(err.message);
     }
+  };
 
-    handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await userService.signup(this.state);
-            this.props.handleSignupOrLogin();
-           
-            this.props.history.push("/");
-        } catch (err) {
-          
-            this.props.updateMessage(err.message);
-        }
-    }
+  const isFormInvalid = () => {
+    return !(
+      formData.name &&
+      formData.email &&
+      formData.password === formData.passwordConf
+    );
+  };
 
-    isFormInvalid() {
-        return !(this.state.name && this.state.email && this.state.password === this.state.passwordConf);
-    }
-
-
-    
-        return (
-            <div>
-                <header className="header-footer">Sign Up</header>
-                <form className="form-horizontal" onSubmit={this.handleSubmit} >
-                    <div className="form-group">
-                        <div className="col-sm-12">
-                            <input type="text" className="form-control" placeholder="Name" value={this.state.name} name="name" onChange={this.handleChange} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-12">
-                            <input type="email" className="form-control" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-12">
-                            <input type="password" className="form-control" placeholder="Password" value={this.state.password} name="password" onChange={this.handleChange} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-12">
-                            <input type="password" className="form-control" placeholder="Confirm Password" value={this.state.passwordConf} name="passwordConf" onChange={this.handleChange} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-12 text-center">
-                            <button className="btn btn-default" disabled={this.isFormInvalid()}>Sign Up</button>&nbsp;&nbsp;
-              <Link to="/">Cancel</Link>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        );
-        }
-
+  return (
+    <div>
+      <header className="header-footer">Sign Up</header>
+      <form className="form-horizontal" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <div className="col-sm-12">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Name"
+              value={formData.name}
+              name="name"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-12">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              value={formData.email}
+              name="email"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-12">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              value={formData.password}
+              name="password"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-12">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Confirm Password"
+              value={formData.passwordConf}
+              name="passwordConf"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-12 text-center">
+            <button className="btn btn-default" disabled={isFormInvalid()}>
+              Sign Up
+            </button>
+            &nbsp;&nbsp;
+            <Link to="/">Cancel</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SignupForm;

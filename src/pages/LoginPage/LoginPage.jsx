b72a2989ef20e04e52a3d890f91bc7f5c36aac3e
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-17 14:06:31 -0400

    adding Signup Page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,62 +1,69 @@
-import React, { useState } from 'react';
-import React, {Component} from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./LoginPage.css";
 import userService from "../../utils/userService";
 
-function LoginPage() {
+const LoginPage = ({ handleSignupOrLogin, history }) => {
+  const [formData, setFormData] = useState({
+    email: "",
+    pw: "",
+  });
 
-    const [email, setEmail]= useState( '')
-      const [pw, setPw]= useState('')
-       
-  
+  const handleChange = (e) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-    handleChange = (e) => {
-     
-        this.setState({
-           
-            [e.target.name]: e.target.value
-        });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await userService.login(formData);
+      handleSignupOrLogin();
+      history.push("/");
+    } catch (err) {
+      alert("Invalid login");
     }
+  };
 
-    handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await userService.login(this.state);
-            this.props.handleSignupOrLogin();
-           
-            this.props.history.push("/");
-        } catch (err) {
-           
-            alert("Invalid login");
-        }
-    }
-
-    
-        return (
-            <div className="LoginPage">
-                <header className="header-footer">Log In</header>
-                <form className="form-horizontal" onSubmit={this.handleSubmit} >
-                    <div className="form-group">
-                        <div className="col-sm-12">
-                            <input type="email" className="form-control" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-12">
-                            <input type="password" className="form-control" placeholder="Password" value={this.state.pw} name="pw" onChange={this.handleChange} />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <div className="col-sm-12 text-center">
-                            <button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;
-                            <Link to="/">Cancel</Link>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        );
-    }
-
+  return (
+    <div className="LoginPage">
+      <header className="header-footer">Log In</header>
+      <form className="form-horizontal" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <div className="col-sm-12">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Email"
+              value={formData.email}
+              name="email"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-12">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+              value={formData.pw}
+              name="pw"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="form-group">
+          <div className="col-sm-12 text-center">
+            <button className="btn btn-default">Log In</button>&nbsp;&nbsp;&nbsp;
+            <Link to="/">Cancel</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default LoginPage;
---
+++ src/pages/SignupPage/SignupPage.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import SignupForm from "../../components/SignupForm/SignupForm";
+import "./SignupPage.css";
+
+const SignupPage = (props) => {
+  const [message, setMessage] = useState("");
+
+  const updateMessage = (msg) => {
+    setMessage(msg);
+  };
+
+  return (
+    <div className="SignupPage">
+      <SignupForm {...props} updateMessage={updateMessage} />
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default SignupPage;
